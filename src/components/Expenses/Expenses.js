90878c0a3fe8939e823a5cf7bcccf15849017558
rishabhgl <rgoyal2_be21@thapar.edu>
2023-01-08 19:21:09 +0530

    Expense list added

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,16 @@
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = (props) => {
+
+    const expenses = props.expenses;
+    return (<Card className="expenses">
+        <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} />
+        <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount} />
+        <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount} />
+        <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount} />
+    </Card>);
+}
+
+export default Expenses;
